836da0e75a99140d89348f4d9615f1997f813abe
monkeyhlj <1466926982@qq.com>
2021-05-05 20:37:39 +0800

    add knn

---
+++ MachineLearningCourseWork/SVM-master/SVM/Test.py
@@ -0,0 +1,138 @@
+# coding:utf-8
+"""
+作者：zhaoxingfeng	日期：2016.12.15
+功能：两个测试样本集对SVM进行性能测试
+    1、UCI公共库wine数据集（1，2），二类分类
+    2、手写字数据库digits（0，1，2），多类分类
+"""
+from __future__ import division
+import numpy as np
+from SVM import *
+from sklearn.cross_validation import train_test_split
+import sys
+if sys.version_info[0] >= 3:
+    xrange = range
+
+# UCI数据集wine
+def wine():
+    def loadDataSet(fileName):
+        dataMat, labelMat = [], []
+        with open(fileName) as fr:
+            for line in fr.readlines():
+                lineArr = line.strip().split(',')
+                dataMat.append([float(data) for data in lineArr[:-1]])
+                if int(lineArr[-1]) == 1:
+                    labelMat.append(1)
+                else:
+                    labelMat.append(-1)
+        return np.mat(dataMat), np.mat(labelMat).T
+
+    print("Step1: Loading data......")
+    filename = r'wine.txt'
+    dataMat, labelMat = loadDataSet(filename)
+    train_x, test_x, train_y, test_y = train_test_split(dataMat, labelMat, test_size = 0.2)
+
+    print("Step2: Training SVM......")
+    C = 20
+    toler = 0.001
+    maxIter = 50
+    kernelOption = ("rbf", 20)
+    svmClassifier = trainSVM(train_x, train_y, C, toler, maxIter, kernelOption)
+
+    print("Step3: Testing classifier......")
+    accuracy, labelpredict, num = testSVM(svmClassifier, test_x, test_y)
+    print("\tAccuracy = %.2f%%" % (accuracy * 100))
+wine()
+
+print("------------------------------------------------------------------")
+
+# 手写字digits, 0/1/2
+def handwriting():
+    def vector(filename):
+        vector1024 = np.zeros((1, 1024))
+        fr = open(filename)
+        for i in range(32):
+            linestr = fr.readline()
+            for j in range(32):
+                vector1024[0, 32 * i + j] = int(linestr[j])
+        return vector1024
+
+    def loadfiles(dirName):
+        from os import listdir
+        fileList = listdir(dirName)
+        labelMat = []
+        dataMat = np.zeros((len(fileList), 1024))
+        for i in range(len(fileList)):
+            num = int(fileList[i].split('_')[0])
+            labelMat.append(num)
+            dataMat[i, :] = vector('%s\%s' % (dirName, fileList[i]))
+        return np.mat(dataMat), np.mat(labelMat).T
+
+    print("Step1: Loading data......")
+    traindataMat, trainlabelMat = loadfiles(r"trainingDigits")
+    testdataMat, testlabelMat = loadfiles(r"testDigits")
+
+    print("Step2: Training SVM......")
+    C = 200
+    toler = 0.0001
+    maxIter = 100
+    kernelOption = ("rbf", 10)
+    # 第一个分类器：0 / 12
+    trainlabel012 = []
+    for i in range(len(trainlabelMat)):
+        if trainlabelMat[i] == 0:
+            trainlabel012.append(-1)
+        else:
+            trainlabel012.append(1)
+    trainlabel012Mat = np.mat(trainlabel012).T
+    traindata012Mat = traindataMat[::]
+    svmClassifier012 = trainSVM(traindata012Mat, trainlabel012Mat, C, toler, maxIter, kernelOption)
+    # 第二个分类器：1 / 2
+    trainlabel12 = []
+    for i in range(trainlabel012.count(-1),len(trainlabelMat)):
+        if trainlabelMat[i] == 1:
+            trainlabel12.append(-1)
+        else:
+            trainlabel12.append(1)
+    trainlabel12Mat = np.mat(trainlabel12).T
+    traindata12Mat = traindataMat[trainlabel012.count(-1):]
+    svmClassifier12 = trainSVM(traindata12Mat, trainlabel12Mat, C, toler, maxIter, kernelOption)
+
+    print("Step3: Testing classifier......")
+    # 测试样本 0 / 12
+    testlabel012 = []
+    for i in range(len(testlabelMat)):
+        if testlabelMat[i] == 0:
+            testlabel012.append(-1)
+        else:
+            testlabel012.append(1)
+    testlabel012Mat = np.mat(testlabel012).T
+    testdata012Mat = testdataMat[::]
+    accuracy012, labelpredict012, numright012 = testSVM(svmClassifier012, testdata012Mat, testlabel012Mat)
+    # 测试样本 1 / 2
+    # 被前一个分类器分为 1 且不包含本来是 -1 结果被分类为 1 的样本
+    testlabel12index = []
+    for i in range(len(labelpredict012)):
+        if labelpredict012[i] == 1 and testlabel012Mat[i] == 1:
+            testlabel12index.append(i)
+    num12 = len(testlabel12index)
+    testlabel12raw = testlabelMat[testlabel12index]
+    testlabel12 = [0 for i in range(num12)]
+    for i in range(num12):
+        if testlabel12raw[i] == 1:
+            testlabel12[i] = -1
+        else:
+            testlabel12[i] = 1
+    testlabel12Mat = np.mat(testlabel12).T
+    testdata12Mat = testdataMat[testlabel12index]
+    accuracy12, labelpredict12, numright12 = testSVM(svmClassifier12, testdata12Mat, testlabel12Mat)
+    """
+    统计被正确分类的样本数量，包括：
+    1、第一轮分类 -> 本来是-1结果被正确分类为-1的个数。
+       第一轮分类结束后，本来是1结果被正确分类为1的样本进入第二轮分类；
+       本来是-1结果本分类为1的样本已经分类错误，不参与第二轮分类
+    2、第二轮分类 -> -1、1被正确分类的个数。
+    """
+    total_accuracy = (numright012 + num12 * accuracy12) / len(testlabelMat)
+    print("\tFinal accuracy = %.3f%%" % (total_accuracy * 100))
+handwriting()
--- README.md
+++ README.md
@@ -3,10 +3,78 @@
 ## 目录
 
 #### 决策树中的分类树1
+    引入了graphviz，画图报错：https://blog.csdn.net/weixin_42067800/article/details/100751057
+    [*zip(feature_name,clf.feature_importances_)]  #连成元组
+    用random_state这个参数来设立随机数种子（随机模式20）因为之前它是随机选取一组特征来进行不纯度的计算
+    在这里不得不提的是，所有接口中要求输入X_train和X_test的部分，输入的特征矩阵必须至少是一个二维矩阵。
+sklearn不接受任何一维矩阵作为特征矩阵被输入。如果你的数据的确只有一个特征，那必须用reshape(-1,1)来给
+矩阵增维；如果你的数据只有一个特征和一个样本，使用reshape(1,-1)来给你的数据增维。
+
 #### 决策树中的回归树2
+    #用numpy来生成一个随机数种子，跟分类树中的random_state差不多
+    rng = np.random.RandomState(1)
+    #rng.rand(10)，生成0-1之间的10个数；rng.rand(2,3),生成2*3矩阵
+    #为什么要生成80*1的二维矩阵，因为训练集和测试集在训练时不能传入一维数组
+    #5*，，变成0-5之间的80个数
+    #np.sort排序，axis=0，y轴列
+    X = np.sort(5 * rng.rand(80,1),axis=0)
+    #用numpy中的正弦函数,此时y也是（80,1）
+    #ravel()降维，降维后(80,)
+    y = np.sin(X).ravel()
+    y[::5]  #取出16个数  80/5=16
+    #加上噪声
+    #y[行:列:步长]  y[::5]所有行所有列每5个取一个数，给这16个数随机的加上一个数
+    #rng.rand(16)：0-1之间  0.5-rng.rand(16)：-0.5-0.5之间  3*：扩大一点噪声：-1.5-1.5之间
+    y[::5] += 3 * (0.5 - rng.rand(16))
+
+    #np.arange(开始点，结束点，步长) []后面是一种增维的用法（见上例子）
+    X_test = np.arange(0.0,5.0,0.01)[:,np.newaxis]
+    X_test
+
+
 #### 决策树中的回归树2（泰坦尼克号幸存者预测）
+    data = pd.read_csv("./datasets/train.csv")
+    data.info()  #查看数据的详细信息，可以看到有缺失值
+    #删选特征 inplace=True会覆盖原表 =False则不会覆盖，但是要生成一个新的对象
+    #axis=1删除列
+    data = data.drop(['Cabin',"Name","Ticket"],inplace=False,axis=1)  
+
+    #补充：当axis=0时，要删除指定行可以用labels标签，例如labels=[2,3]删除2、3行
+    
+    #处理缺失值
+    data["Age"] = data["Age"].fillna(data["Age"].mean()) #用均值去填补
+    #Embarked只有两行缺失值，直接把这两行删掉
+    #删掉有缺失值的行
+    data = data.dropna(axis=0)
+    
+    labels = data["Embarked"].unique().tolist()
+    #apply()在这一列里执行括号里面的操作
+    #把Embarked里面的值转换成她对应得索引（变成数字）
+    data["Embarked"] = data["Embarked"].apply(lambda x:labels.index(x))
+    #把True和False变成0 1
+    (data["Sex"] == "male").astype("int")
+    #性别也可以用Embarked的方法来转换，这里用一种新的方法
+    # data["Sex"] = (data["Sex"] == "male").astype("int")
+    data.loc[:,"Sex"] = (data["Sex"] == "male").astype("int")
+    
+    data.iloc[:,3] #取出Sex这一列
+    
+    x = data.iloc[:,data.columns != "Survived"]
+    
+    #把索引恢复
+    for i in [Xtrain,Xtest,Ytrain,Ytest]:
+        i.index = range(i.shape[0])
+    
+    学习曲线
+    网格搜索
+
 #### 随机森林1（分类）
 #### 随机森林2（回归）
+    用随机森林回归填补缺失值
+    plt.figure(figsize=(12,6)) #画出画布
+    ax = plt.subplot(111) #plt.subplot添加子图 111 第一行第一列第一个表
+
+    
 #### 机器学习中调参的基本思想
 #### 随机森林实例：在乳腺癌数据上的调参
 #### 特征工程1-数据预处理
